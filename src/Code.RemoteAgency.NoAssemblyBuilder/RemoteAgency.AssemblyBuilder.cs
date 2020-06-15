a3011f0a85226a5e4c5a651cd736973c9e235790
Allen Cui <allen_st_clair@msn.com>
2020-06-15 20:11:55 +0800

    building

---
+++ src/Code.RemoteAgency.NoAssemblyBuilder/RemoteAgency.AssemblyBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretNest.RemoteAgency
+{
+    public abstract partial class RemoteAgency
+    {
+        bool BuildAssembly(string assemblyName, IEnumerable<string> sourceCode, IEnumerable<AssemblyReference> references, out byte[] assemblyImage, out TypeCreatingException buidlingError)
+        {
+            throw new NotSupportedException("Neat version of RemoteAgency does not shipped with built-in assembly builder.");
+        }
+    }
+}
