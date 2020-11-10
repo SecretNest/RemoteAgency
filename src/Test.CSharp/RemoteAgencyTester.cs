46964b4e8f5c97353197ef08c3f3d1f52ba062b2
Allen Cui <allen_st_clair@msn.com>
2020-11-10 14:22:07 +0800

    tester

--- src/Test.CSharp/RemoteAgencyTester.cs
+++ src/Test.CSharp/RemoteAgencyTester.cs
@@ -13,19 +13,42 @@ namespace Test.CSharp
 
         public Guid RemoteAgencyInstanceId => _remoteAgency.SiteId;
 
+        public event EventHandler<PreparedMessage> MessagePrepared;
+
         public RemoteAgencyTester()
         {
-
+            _remoteAgency.MessageForSendingPrepared += _remoteAgency_MessageForSendingPrepared;
         }
 
-        //public TServiceContractInterface CreateProxy(Guid targetSiteId, Guid targetInstanceId, ref Guid instanceId)
-        //{
-        //    return _remoteAgency.CreateProxy<TServiceContractInterface>(targetSiteId, targetInstanceId, ref instanceId);
-        //}
+        public TServiceContractInterface CreateProxy(Guid targetSiteId, Guid targetInstanceId, out Guid instanceId)
+        {
+            return _remoteAgency.CreateProxy<TServiceContractInterface>(targetSiteId, targetInstanceId, out instanceId);
+        }
 
-        //public Guid CreateServiceWrapper(TServiceContractInterface serviceObject, ref Guid instanceId)
-        //{
-        //    return _remoteAgency.CreateServiceWrapper(serviceObject, ref instanceId);
-        //}
+        public Guid CreateServiceWrapper(TServiceContractInterface serviceObject)
+        {
+            return _remoteAgency.CreateServiceWrapper(serviceObject);
+        }
+
+        private void _remoteAgency_MessageForSendingPrepared(object sender, MessageBodyEventArgs<string, object> e)
+        {
+            PreparedMessage message = new PreparedMessage()
+            {
+                Message = e.Serialize(),
+                TargetSiteId = e.TargetSiteId
+            };
+            MessagePrepared?.Invoke(this, message);
+        }
+
+        public void ProcessMessage(string message)
+        {
+            _remoteAgency.ProcessReceivedSerializedMessage(message);
+        }
+    }
+
+    public class PreparedMessage : EventArgs
+    {
+        public string Message { get; set; }
+        public Guid TargetSiteId { get; set; }
     }
 }
