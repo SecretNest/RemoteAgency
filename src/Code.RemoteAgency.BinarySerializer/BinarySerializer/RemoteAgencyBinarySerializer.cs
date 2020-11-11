06ed4a04b232ef00e18f5aeea5158ee44dd00270
Allen Cui <allen_st_clair@msn.com>
2020-11-11 15:18:56 +0800

    Add dotnet5 support. Add security warning for BinaryFormatter.

--- src/Code.RemoteAgency.BinarySerializer/BinarySerializer/RemoteAgencyBinarySerializer.cs
+++ src/Code.RemoteAgency.BinarySerializer/BinarySerializer/RemoteAgencyBinarySerializer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Security;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -10,6 +11,7 @@ namespace SecretNest.RemoteAgency.BinarySerializer
     /// <summary>
     /// Provides binary based serializing and deserializing methods for entities.
     /// </summary>
+    /// <note type="warning">BinaryFormatter is included in this serializer, which is dangerous and should not be used. See <conceptualLink target="886b6555-5b60-46ed-b0e3-aa383c95108c" >Binary Formatter Warning</conceptualLink> for details.</note>
     public class RemoteAgencyBinarySerializer: SerializingHelperBase<byte[], object>
     {
         readonly BinaryFormatter _formatter;
@@ -17,8 +19,11 @@ namespace SecretNest.RemoteAgency.BinarySerializer
         /// <summary>
         /// Initializes an instance of RemoteAgencyBinarySerializer.
         /// </summary>
-        public RemoteAgencyBinarySerializer()
+        /// <param name="securityIssueAcknowledged">This value needs to be set to <see langword="true" /> to confirm that the security issue is known, or an exception will be thrown. Default value is <see langword="false"/>.</param>
+        public RemoteAgencyBinarySerializer(bool securityIssueAcknowledged = false)
         {
+            if (!securityIssueAcknowledged)
+                throw new SecurityException("BinaryFormatter is included in this serializer, which is dangerous and should not be used.");
             _formatter = new BinaryFormatter();
         }
 
@@ -27,7 +32,9 @@ namespace SecretNest.RemoteAgency.BinarySerializer
         {
             using (var stream = new MemoryStream())
             {
+#pragma warning disable SYSLIB0011
                 _formatter.Serialize(stream, original);
+#pragma warning restore SYSLIB0011
                 return stream.ToArray();
             }
         }
@@ -37,7 +44,9 @@ namespace SecretNest.RemoteAgency.BinarySerializer
         {
             using (var stream = new MemoryStream(serialized))
             {
+#pragma warning disable SYSLIB0011
                 return _formatter.Deserialize(stream);
+#pragma warning restore SYSLIB0011
             }
         }
     }
--- src/Code.RemoteAgency.BinarySerializer/RemoteAgency.Factory.cs
+++ src/Code.RemoteAgency.BinarySerializer/RemoteAgency.Factory.cs
@@ -10,11 +10,13 @@ namespace SecretNest.RemoteAgency
         /// <summary>
         /// Creates an instance of Remote Agency using binary serializer.
         /// </summary>
+        /// <param name="securityIssueAcknowledged">This value needs to be set to <see langword="true" /> to confirm that the security issue is known, or an exception will be thrown. Default value is <see langword="false"/>.</param>
         /// <param name="siteId">Site id. A randomized value is used when it is set to <see cref="Guid"/>.Empty or absent.</param>
         /// <returns>Created Remote Agency instance.</returns>
-        public static RemoteAgency<byte[]> CreateWithBinarySerializer(Guid? siteId = null)
+        /// <note type="warning">BinaryFormatter is included in this serializer, which is dangerous and should not be used. See <conceptualLink target="886b6555-5b60-46ed-b0e3-aa383c95108c" >Binary Formatter Warning</conceptualLink> for details.</note>
+        public static RemoteAgency<byte[]> CreateWithBinarySerializer(bool securityIssueAcknowledged = false, Guid? siteId = null)
         {
-            return (RemoteAgency<byte[]>)CreateWithoutCheck<byte[], object>(new RemoteAgencyBinarySerializer(), new RemoteAgencyBinarySerializerEntityTypeBuilder(), siteId);
+            return (RemoteAgency<byte[]>)CreateWithoutCheck<byte[], object>(new RemoteAgencyBinarySerializer(securityIssueAcknowledged), new RemoteAgencyBinarySerializerEntityTypeBuilder(), siteId);
         }
     }
 }
