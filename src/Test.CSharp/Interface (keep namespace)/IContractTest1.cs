16c7693d46fcdb18de0cf23d83a817a9ab7eb0d3
Allen Cui <allen_st_clair@msn.com>
2020-12-08 13:05:00 +0800

    test code

---
+++ src/Test.CSharp/Interface (keep namespace)/IContractTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SecretNest.RemoteAgency.Attributes;
+
+namespace Test.CSharp
+{
+    public interface IContractTest1
+    {
+        int Add(int a, int b);
+
+        //[CustomizedAssetName("AddDouble")]
+        float Add(float a, float b);
+
+        [CustomizedAssetName("AddDouble")]
+        double Add(double a, double b);
+    }
+}
