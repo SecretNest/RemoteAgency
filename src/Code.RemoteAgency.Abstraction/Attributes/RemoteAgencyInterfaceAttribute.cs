59533c1f93de6adb402b06d50a59e391906a6191
Allen Tsui <allen_st_clair@msn.com>
2024-11-25 15:44:00 +0800

    Update RemoteAgencyInterfaceAttribute.cs

--- src/Code.RemoteAgency.Abstraction/Attributes/RemoteAgencyInterfaceAttribute.cs
+++ src/Code.RemoteAgency.Abstraction/Attributes/RemoteAgencyInterfaceAttribute.cs
@@ -6,7 +6,7 @@ namespace SecretNest.RemoteAgency.Attributes
     /// Indicates this interface should be processed by Remote Agency.
     /// </summary>
     /// <remarks>
-    /// This attribute has no use in this release. Future releases may preprocess the Interface that marks this attribute to enable acceleration.
+    /// This attribute has no use in this release. Future releases may preprocess the interfaces those marked with this attribute to enable acceleration.
     /// </remarks>
     [AttributeUsage(AttributeTargets.Interface, Inherited = true, AllowMultiple = true)]
     public class RemoteAgencyInterfaceAttribute : Attribute
